27888e5cdb86ebf1b233df9942721e8ec6a19506
Marcel Heers <info@heers.it>
2020-11-13 16:01:34 +0100

    can switch the theme via url

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -6,10 +6,14 @@
 
 <script>
 import { mapMutations } from "vuex";
+import { baseURL } from "@/utils/constants";
+
 export default {
   name: "app",
   mounted() {
     this.updateLocale();
+    this.updateCSS();
+
     const loading = document.getElementById("loading");
     loading.classList.add("done");
 
@@ -22,6 +26,26 @@ export default {
     updateLocale() {
       if (Object.prototype.hasOwnProperty.call(this.$route.query, "locale")) {
         this.updateUser({ locale: this.$route.query.locale });
+      }
+    },
+    updateCSS() {
+      if (Object.prototype.hasOwnProperty.call(this.$route.query, "theme")) {
+        const theme = this.$route.query.theme;
+        if (!theme || theme === "") {
+          return;
+        }
+        const customCssID = "custom-styles";
+        if (document.getElementById(customCssID) === null) {
+          const style = document.createElement("link");
+          style.rel = "stylesheet";
+          style.href = `${baseURL}/static/themes/${theme}.css`;
+          style.id = customCssID;
+          // Get the first script tag
+          const ref = document.querySelector("#endOfBody");
+
+          // Insert our new styles before the first script tag
+          ref.parentNode.insertBefore(style, ref);
+        }
       }
     },
   },

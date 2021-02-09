ad9db1b4f65d747f51fceb5038f7c07f0e4b3650
Marcel Heers <info@heers.it>
2021-02-09 10:34:12 +0100

    fixes handling for files that contain some special chars like '(' or ')'

--- frontend/src/api/utils.js
+++ frontend/src/api/utils.js
@@ -1,48 +1,56 @@
-import store from '@/store'
-import { renew } from '@/utils/auth'
-import { baseURL } from '@/utils/constants'
+import store from "@/store";
+import { renew } from "@/utils/auth";
+import { baseURL } from "@/utils/constants";
 
-export async function fetchURL (url, opts) {
-  opts = opts || {}
-  opts.headers = opts.headers || {}
+export async function fetchURL(url, opts) {
+  opts = opts || {};
+  opts.headers = opts.headers || {};
 
-  let { headers, ...rest } = opts
+  let { headers, ...rest } = opts;
 
   const res = await fetch(`${baseURL}${url}`, {
     headers: {
-      'X-Auth': store.state.jwt,
-      'Authorization': 'Bearer ' + localStorage.getItem('id_token'),
-      ...headers
+      "X-Auth": store.state.jwt,
+      Authorization: "Bearer " + localStorage.getItem("id_token"),
+      ...headers,
     },
-    ...rest
-  })
+    ...rest,
+  });
 
-  if (res.headers.get('X-Renew-Token') === 'true') {
-    await renew(store.state.jwt)
+  if (res.headers.get("X-Renew-Token") === "true") {
+    await renew(store.state.jwt);
   }
 
-  return res
+  return res;
 }
 
-export async function fetchJSON (url, opts) {
-  const res = await fetchURL(url, opts)
+export async function fetchJSON(url, opts) {
+  const res = await fetchURL(url, opts);
 
   if (res.status === 200) {
-    return res.json()
+    return res.json();
   } else {
-    throw new Error(res.status)
+    throw new Error(res.status);
   }
 }
 
-export function removePrefix (url) {
-  if (url.startsWith('/files')) {
-    url = url.slice(6)
-  } else if (store.getters['isSharing']) {
-    url = url.slice(7 + store.state.hash.length)
+export function removePrefix(url) {
+  if (url.startsWith("/files")) {
+    url = url.slice(6);
+  } else if (store.getters["isSharing"]) {
+    url = url.slice(7 + store.state.hash.length);
   }
 
-  if (url === '') url = '/'
-  if (url[0] !== '/') url = '/' + url
-  return url
+  if (url === "") url = "/";
+  if (url[0] !== "/") url = "/" + url;
+
+  url = replaceSpecialChars(url);
+
+  return url;
 }
 
+export function replaceSpecialChars(str) {
+  return str.replace(/[!'()*]/g, function(c) {
+    return "%" + c.charCodeAt(0).toString(16);
+  });
+}

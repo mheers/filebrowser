3fa4d3f7b6ba7cd77f62f0b54212b4ff87fc37e7
Marcel Heers <info@heers.it>
2021-06-08 08:47:59 +0200

    Revert "feat: gzip encoding for static js files"
    This reverts commit aa172b8bb5f17d5f5cb9666bfb5ee650d8091fb5.

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,15 +1,5 @@
-const CompressionPlugin = require("compression-webpack-plugin");
-
 module.exports = {
   runtimeCompiler: true,
-  publicPath: "/static/",
+  publicPath: "[{[ .StaticURL ]}]",
   parallel: 2,
-  configureWebpack: {
-    plugins: [
-      new CompressionPlugin({
-        include: /\.js$/,
-        deleteOriginalAssets: true,
-      }),
-    ],
-  },
 };

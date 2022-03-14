735ac940107061e7752e933346aa34466e3d95cb
Marcel Heers <info@heers.it>
2022-03-14 17:23:45 +0200

    can use local frontend and local backend via proxy

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,4 +2,16 @@ module.exports = {
   runtimeCompiler: true,
   publicPath: "[{[ .StaticURL ]}]",
   parallel: 2,
+  devServer: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:8095/api",
+        changeOrigin: true,
+        pathRewrite: {
+          "^/api": "",
+        },
+        ws: true,
+      },
+    },
+  },
 };
